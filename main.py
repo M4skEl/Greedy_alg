899b0bcb55d6ee0ca1ad08361b61b2da6f0f5f8a
M4skEl <sivereliseu7@gmail.com>
2021-12-25 12:35:03 +0300

    delete strings

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import sys
 
 
 def one_counter(str):
-    return str.count('1')
+    count = 0
+    while str != 0:
+        if str % 2 == 1:
+            count += 1
+        str = int(str / 2)
+    return count
 
 
 def zero_count(str):
     count = 0
-    for i in range(len(str)):
-        if str[len(str) - 1 - i] == '0':
-            count += 1
-        if str[len(str) - 1 - i] == '1':
-            break
+    while str % 2 == 0:
+        str = str / 2
+        count += 1
     return count
-
-
-def made_bin(number):
-    str = bin(number)
-    return str[2:]
 
 
 def main():
@@ -25,23 +23,26 @@ def main():
     sum = int(input())
 
     while sum != 0:
-        half = made_bin(sum)
-        if half[-1] == '0':
+        half = bin(sum)
+        if sum % 2 == 0:
             sum = int(sum / 2)
             command_list.append('dbl')
-        elif half[-1] == '1':
-            count_1 = one_counter(made_bin(sum - 1))
-            count_2 = one_counter(made_bin(sum + 1))
+        else:
+            count_1 = one_counter(sum - 1)
+            count_2 = one_counter(sum + 1)
+
             if count_1 < count_2:
                 sum = sum - 1
                 command_list.append('inc')
+
             elif count_1 > count_2:
                 sum = sum + 1
                 command_list.append('dec')
+
             elif count_1 == count_2:
-                zero_count_1 = zero_count(made_bin(sum - 1))
-                zero_count_2 = zero_count(made_bin(sum + 1))
-                if zero_count_1 < zero_count_2 and len(made_bin(sum + 1)) <= len(made_bin(sum - 1)):
+                zero_count_1 = zero_count(sum - 1)
+                zero_count_2 = zero_count(sum + 1)
+                if zero_count_1 < zero_count_2 and len(bin(sum + 1)) <= len(bin(sum - 1)):
                     sum = sum + 1
                     command_list.append('dec')
                 else:
